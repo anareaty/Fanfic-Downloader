9416f36901864f9bab73e1ad1e46ba5cc1c4a179
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2022-10-21 16:19:22 +0400

    Убираю функции фикбука

--- options.js
+++ options.js
@@ -1,43 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  chrome.storage.local.get(["Ao3", "FB", "Ao3Format", "FBFormat"], result => {
+  chrome.storage.local.get(["Ao3", "Ao3Format"], result => {
     if (result.Ao3 == true) {
       document.getElementById("Ao3").checked = true;
     } else {
       document.getElementById("Ao3").checked = false;
     }
 
-    if (result.FB == true) {
-      document.getElementById("FB").checked = true;
-    } else {
-      document.getElementById("FB").checked = false;
-    }
+   
+   
+  
+  
+
 
     if (result.Ao3Format != undefined) {
       document.querySelector("#Ao3-format > label:nth-child(" + result.Ao3Format + ") > input[type=radio]").checked = true;
     }
-    if (result.FBFormat != undefined) {
-      document.querySelector("#FB-format > label:nth-child(" + result.FBFormat + ") > input[type=radio]").checked = true;
-    }
+  
+   
+ 
   })
 
   document.getElementById("Ao3").onchange = () => {
   chrome.storage.local.set({
     "Ao3": document.getElementById("Ao3").checked})
   }
-  document.getElementById("FB").onchange = () => {
-    chrome.storage.local.set({
-      "FB": document.getElementById("FB").checked})
-  }
+  
+  
+  
+  
+  
+  
+  
 
+
+  
+  
   document.getElementById("Ao3-format").onclick = () => {
     chrome.storage.local.set({
       "Ao3Format": document.getElementById("Ao3-format").elements['Ao3-format'].value})
   }
 
-  document.getElementById("FB-format").onclick = () => {
-    chrome.storage.local.set({
-      "FBFormat": document.getElementById("FB-format").elements['FB-format'].value})
-  }
+  
+  
+  
+
+
+
+ 
+  
+  
+  
 
 })
