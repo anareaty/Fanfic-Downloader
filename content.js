d47f58ba1b5eeaf882756870d5ad4aa4964bcfbc
Anastasia Fedotova <55949830+anareaty@users.noreply.github.com>
2022-10-21 16:53:03 +0400

    Убираю функции фикбука (#1)
    * Убираю функции фикбука
    
    * Убираю функции фикбука
    
    * Убираю функции фикбука
    
    * Убираю функции фикбука
    
    * Убираю функции фикбука
    
    * Убираю функции фикбука
    
    * Убираю экспериментальную функцию
    
    * Убираю экспериментальную функцию
    
    * Убираю экспериментальную функцию (скачивание обновлений)

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
-chrome.storage.local.get(["Ao3", "FB", "Ao3Format", "FBFormat", "extensionOn"], result => {
+chrome.storage.local.get(["Ao3", "Ao3Format", "extensionOn"], result => {
   if (result.extensionOn != false) {
 
     let Ao3Format = result.Ao3Format;
-    let FBFormat = result.FBFormat;
+
 
     if (Ao3Format === undefined) Ao3Format = 2;
-    if (FBFormat === undefined) FBFormat = 2;
+    
 
 
     //Форматы Ao3: 1-AZW3, 2-EPUB, 3-MOBI, 4-PDF, 5-HTML
@@ -16,17 +16,7 @@ chrome.storage.local.get(["Ao3", "FB", "Ao3Format", "FBFormat", "extensionOn"], 
     }
 
 
-    //Фикбук
-    //Автоматические скачивать при открытии страницы загрузки
-    if (/https:\/\/ficbook\.net\/readfic.*download/.test(location.href)) {
-      document.querySelector("#main > div.main-holder.alt > section > div > section:nth-child("+(FBFormat+1)+") > div > form > button").click()
-      setTimeout(() => {window.close()}, 3000);
-    }
 
-    //Открывать страницу загрузки при открытии страницы фанфика
-    if (/https:\/\/ficbook\.net\/readfic.*/.test(location.href) && !/download/.test(location.href) && result.FB != false) {
-      window.open(location.href.replace(/(.*)(\?)(.*)/, "$1") + "/download")
-    }
 
   }
 });
