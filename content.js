2d3e0753f91ed0cdc8c03d25a679429522215343
anareaty <reatyrabota@yandex.ru>
2022-10-23 09:17:12 +0400

    Исправление бага с закрытием вкладок

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 chrome.storage.local.get(["Ao3", "Ao3Format", "extensionOn", "bookmarks"], result => {
-
+alert (window.closable)
   if (result.extensionOn != false) {
 
     // Скачивание файла при открытии страницы
@@ -15,33 +15,42 @@ chrome.storage.local.get(["Ao3", "Ao3Format", "extensionOn", "bookmarks"], resul
     // Добавление в закладки при открытии страницы
     if (result.bookmarks == true) {
       // Проверяем, не заблокирована ли страница
-      let pageBlocked = /Retry later/.test(document.querySelector('body').innerText)
+//      let pageBlocked = /Retry later/.test(document.querySelector('body').innerText)
       // Добавляем закладку
       if (/archiveofourown\.org\/works.*/.test(location.href)) {
-        let bookmarkButton = document.getElementsByClassName("bookmark_form_placement_open")[0]
-        if (bookmarkButton.innerHTML == "Bookmark") {
-          bookmarkButton.click();
-          let submitButton = document.querySelector("#bookmark-form > form > fieldset > fieldset > p > input")
-          submitButton.form.submit()
-        } else {
-          // Закрываем окно, если закладка уже добавлена
-          if (window.closable && !pageBlocked) window.close();
+          let bookmarkButton = document.getElementsByClassName("bookmark_form_placement_open")[0]
+          if (bookmarkButton.innerHTML == "Bookmark") {
+            // Закладки на этот фик ещё нет
+            // Нажимаем на кнопки для добавления закладки
+            bookmarkButton.click();
+            let submitButton = document.querySelector("#bookmark-form > form > fieldset > fieldset > p > input")
+            submitButton.form.submit()
+          } else {
+            // Закладка уже добавлена
+            // Закрываем вкладку, если она открыта через массовое добавление
+            if (window.closable) window.close();
+            // Если вкладка открыта вручную, ничего не делаем
+          }
         }
+
       }
       // Закрываем страницу после добавления закладки или перенаправляем назад к фанфику
       if (/archiveofourown\.org\/bookmarks\/\d.*/.test(location.href)) {
-        if (!pageBlocked) {
-          if (window.closable) {
-            window.close()
-          } else {
-            let ficLink = document.querySelector("div.header.module > h4 > a");
-            ficLink.click();
-          }
-
-        }
+        window.close()
 
 
-      }
+//        if (true) {
+//          if (window.closable) {
+//            window.close()
+//          } else {
+//            window.history.back()
+//            let ficLink = document.querySelector("div.header.module > h4 > a");
+//            ficLink.click();
+//          }
+//        }
+
+
+
     }
 
   }
